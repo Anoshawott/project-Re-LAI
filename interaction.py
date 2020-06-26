a2c691360bbe4072417c1758a325d13607a748a2
Anosh Samsudeen <54537931+Anoshawott@users.noreply.github.com>
2020-06-27 09:21:03 +1000

    Update interaction.py

--- interaction.py
+++ interaction.py
@@ -4,6 +4,9 @@ import numpy as np
 import pyautogui
 import quartz
 from pynput.mouse import Button, Controller
+import mss
+import mss.tools
+import cv2
 
 # # importing os module  
 # import os 
@@ -41,6 +44,36 @@ mouse = Controller()
 # SCROLLING
 
 # mouse.scroll(up/down, left/right)
+
+
+
+# Following attempts to read and interpret on-screen information
+
+# screenshot() returns an image of some given area
+def screenshot(x, y, width, height
+               , reduction_factor = 1
+               , gray = True
+              ):
+    
+    with mss.mss() as sct:
+        # The screen part to capture
+        region = {'left': x, 'top': y, 'width': width, 'height': height}
+
+        # Grab the data
+        img = sct.grab(region)
+
+        if gray:
+            result = cv2.cvtColor(np.array(img), cv2.COLOR_BGRA2GRAY)
+        else:
+            result = cv2.cvtColor(np.array(img), cv2.COLOR_BGRA2BGR)
+
+        img = result[::reduction_factor, ::reduction_factor]
+        return img
+
+cv2.imshow('img', img)
+cv2.waitkey()
+
+
 
 
 
