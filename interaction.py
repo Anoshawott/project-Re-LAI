a1ab1bb84f346d957cfe43893537c71ffa44b1f8
Anosh Samsudeen <54537931+Anoshawott@users.noreply.github.com>
2020-06-27 09:10:20 +1000

    Update interaction.py

--- interaction.py
+++ interaction.py
@@ -3,6 +3,7 @@
 import numpy as np
 import pyautogui
 import quartz
+from pynput.mouse import Button, Controller
 
 # # importing os module  
 # import os 
@@ -18,51 +19,29 @@ import quartz
 # print("User's Environment variable:") 
 # pprint.pprint(dict(env_var), width = 1) 
 
-# Following class is for controlling the mouse using python
+# Following is for controlling the mouse using python
 
-class Mouse():
-    down = [quartz.kCGEventLeftMouseDown, quartz.kCGEventRightMouseDown, quartz.kCGEventOtherMouseDown]
-    up = [quartz.kCGEventLeftMouseUp, quartz.kCGEventRightMouseUp, quartz.kCGEventOtherMouseUp]
-    [LEFT, RIGHT, OTHER] = [0, 1, 2]
+mouse = Controller()
+# TO GIVE CURRENT POSITION
 
-    def position(self):
-        point = quartz.CGEventGetLocation( quartz.CGEventCreate(None) )
-        return point.x, point.y
+# mouse.position
 
-    def location(self):
-        loc = NSEvent.mouseLocation()
-        return loc.x, quartz.CGDisplayPixelsHigh(0) - loc.y
+# TO MOVE TO EXACT GIVEN PIXEL COORDINATE
 
-    def move(self, x, y):
-        moveEvent = quartz.CGEventCreateMouseEvent(None, quartz.kCGEventMouseMoved, (x, y), 0)
-        quartz.CGEventPost(quartz.kCGHIDEventTap, moveEvent)
+# mouse.position = (x,y)
 
-    def press(self, x, y, button=1):
-        event = quartz.CGEventCreateMouseEvent(None, Mouse.down[button], (x, y), button - 1)
-        quartz.CGEventPost(quartz.kCGHIDEventTap, event)
+# TO MOVE RELATIVE TO THE PROVIDED PARAMETERS OF X AND Y
 
-    def release(self, x, y, button=1):
-        event = quartz.CGEventCreateMouseEvent(None, Mouse.up[button], (x, y), button - 1)
-        quartz.CGEventPost(quartz.kCGHIDEventTap, event)
+# mouse.move(x,y)
 
-    def click(self, button=LEFT):
-        x, y = self.position()
-        self.press(x, y, button)
-        self.release(x, y, button)
+# MOUSE BUTTON CLICKS (LEFT-PARAM: Type of click, RIGHT-PARAM: number of clicks)
 
-    def click_pos(self, x, y, button=LEFT):
-        self.move(x, y)
-        self.click(button)
+# mouse.click(Button.right, 1)
 
-    def to_relative(self, x, y):
-        curr_pos = quartz.CGEventGetLocation( quartz.CGEventCreate(None) )
-        x += current_position.x;
-        y += current_position.y;
-        return [x, y]
+# SCROLLING
 
-    def move_rel(self, x, y):
-        [x, y] = to_relative(x, y)
-        moveEvent = quartz.CGEventCreateMouseEvent(None, quartz.kCGEventMouseMoved, quartz.CGPointMake(x, y), 0)
-        quartz.CGEventPost(quartz.kCGHIDEventTap, moveEvent)
+# mouse.scroll(up/down, left/right)
+
+
 
 print('All works!')
