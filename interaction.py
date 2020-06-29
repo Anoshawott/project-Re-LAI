747679ded437357f4a13f3d9c3a06936d6825420
Anosh Samsudeen <54537931+Anoshawott@users.noreply.github.com>
2020-06-29 22:28:43 +1000

    Digit Recognition Update 2.0

--- interaction.py
+++ interaction.py
@@ -178,51 +178,87 @@ print("Restored model, accuracy: {:5.2f}%".format(100*acc))
 print('digit model passed')
 
 
-# def digit_detection():
-#     # Load the classifier
-#     clf = joblib.load("digits_cls.pkl")
+def digit_detection():
+    # Load the classifier
+    # clf = joblib.load("digits_cls.pkl")
 
-#     # Read the input image 
-#     im = cv2.imread("test2.jpg")
+    # Read the input image 
+    im = cv2.imread("score_test.jpg")
 
-#     # Convert to grayscale and apply Gaussian filtering
-#     im_gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
-#     im_gray = cv2.GaussianBlur(im_gray, (5, 5), 0)
+    # Convert to grayscale and apply Gaussian filtering; was COLOR_BGR2GRAY
+    im_gray = cv2.cvtColor(im, cv2.COLOR_BGR2HSV)
+    # im_gray = cv2.GaussianBlur(im_gray, (5, 5), 0)
 
-#     # Threshold the image
-#     ret, im_th = cv2.threshold(im_gray, 90, 255, cv2.THRESH_BINARY_INV)
+    lower_yellow = np.array([0, 70, 0])
+    upper_yellow = np.array([255, 255, 255])
 
-#     # Find contours in the image
-#     ctrs, hier = cv2.findContours(im_th.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    mask = cv2.inRange(im_gray, lower_yellow, upper_yellow)
+    res = cv2.bitwise_and(im, im, mask = mask)
 
-#     # Get rectangles contains each contour
-#     rects = [cv2.boundingRect(ctr) for ctr in ctrs]
+    cv2.imshow('im', im)
+    cv2.imshow('mask', mask)
+    cv2.imshow('res', res)
 
-#     # For each rectangular region, calculate HOG features and predict
-#     # the digit using Linear SVM.
-#     for rect in rects:
-#         # Draw the rectangles
-#         cv2.rectangle(im, (rect[0], rect[1]), (rect[0] + rect[2], rect[1] + rect[3]), (0, 255, 0), 3) 
-#         # Make the rectangular region around the digit
-#         leng = int(rect[3] * 1.6)
-#         pt1 = int(rect[1] + rect[3] // 2 - leng // 2)
-#         pt2 = int(rect[0] + rect[2] // 2 - leng // 2)
-#         roi = im_th[pt1:pt1+leng, pt2:pt2+leng]
-#         print(pt1)
-#         print(pt2)
-#         print(leng)
-#         # Resize the image
-#         roi = cv2.resize(roi, (8, 8), interpolation=cv2.INTER_AREA)
-#         roi = cv2.dilate(roi, (3, 3))
-#         # Calculate the HOG features
-#         roi_hog_fd = hog(roi, orientations=9, pixels_per_cell=(4, 4), cells_per_block=(1, 1), visualize=False)
-#         nbr = clf.predict(np.array([roi_hog_fd], 'float64'))
-#         cv2.putText(im, str(int(nbr[0])), (rect[0], rect[1]),cv2.FONT_HERSHEY_DUPLEX, 2, (0, 255, 255), 3)
+    cv2.waitKey()
 
-#     cv2.imshow("Resulting Image with Rectangular ROIs", im)
-#     cv2.waitKey()
+    # Getting HSV range
+    # upper_y = np.uint8([[[74,141,134]]])
+    # hsv_upper_y = cv2.cvtColor(upper_y,cv2.COLOR_BGR2HSV)
+    # print( hsv_upper_y )
 
-# digit_detection()
+    print('Yesss!')
+
+    # Inversing the filter
+    ret, im_th = cv2.threshold(res, 0, 255, cv2.THRESH_BINARY_INV)
+    
+    # Find contours in the image
+    ctrs, hier = cv2.findContours(im_th.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    # Get rectangles contains each contour
+    rects = [cv2.boundingRect(ctr) for ctr in ctrs]
+
+    # #create an empty image for contours
+    # img_contours = np.zeros(im.shape)
+    # # draw the contours on the empty image
+    # cv2.drawContours(img_contours, ctrs, -1, (0,255,0), 3)
+    # cv2.imshow('contours', img_contours)
+    # cv2.waitKey()
+
+
+
+    # # For each rectangular region, calculate HOG features and predict
+    # # the digit using Linear SVM.
+    # for rect in rects:
+    #     # Draw the rectangles
+    #     cv2.rectangle(im, (rect[0], rect[1]), (rect[0] + rect[2], rect[1] + rect[3]), (0, 255, 0), 3) 
+    #     # Make the rectangular region around the digit
+    #     leng = int(rect[3] * 1.6)
+    #     pt1 = int(rect[1] + rect[3] // 2 - leng // 2)
+    #     pt2 = int(rect[0] + rect[2] // 2 - leng // 2)
+    #     roi = im_th[pt1:pt1+leng, pt2:pt2+leng]
+    #     # Resize the image
+    #     try:
+    #         roi = cv2.resize(roi, (28, 28), interpolation=cv2.INTER_AREA)
+    #         roi = cv2.dilate(roi, (3, 3))
+    #         roi = tf.keras.utils.normalize(roi, axis=1)
+    #         cv2.imshow("roi", roi)
+    #         cv2.waitKey()
+    #         # Calculate the HOG features
+    #         nbr = new_model.predict([np.array([roi])])
+    #         print(nbr)
+    #         print(np.argmax(nbr))
+    #         cv2.putText(im, str(int(nbr[0])), (rect[0], rect[1]),cv2.FONT_HERSHEY_DUPLEX, 2, (0, 255, 255), 3)
+    #     except:
+    #         continue
+        
+        # print('---------')
+        
+        
+
+    # cv2.imshow("Resulting Image with Rectangular ROIs", im)
+    # cv2.waitKey()
+
+digit_detection()
 
 
 
@@ -386,3 +422,15 @@ def is_dead():
 #             direction = move_to_radians(random.uniform(0, math.pi*2), random.uniform(0,300))
 
 print('All works!')
+
+
+
+##### OLD EROSION TECHNIQUE
+
+# Erode image to create more distinct numbers
+    # kernel = np.ones((4,4), np.uint8)
+    # img_erosion = cv2.erode(im_gray, kernel, iterations=1)
+    # print(img_erosion)
+    # Threshold the image
+    # ret, im_th = cv2.threshold(img_erosion, 57, 255, cv2.THRESH_BINARY)
+    # cv2.imshow('thresh', im_th)
